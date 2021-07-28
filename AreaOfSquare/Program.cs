a4eeadb544641bbea7e65a535ceecb8798149127
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-28 22:47:54 +0530

    Add project files.

---
+++ AreaOfSquare/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AreaOfSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string val;
+            Console.WriteLine("Enter the side of a square");
+            val = Console.ReadLine();
+            double side = Convert.ToDouble(val);
+            Console.WriteLine(side);
+            double area = side * side;
+            Console.WriteLine("Area of square = " + area);
+        }
+    }
+}
